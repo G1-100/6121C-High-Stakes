d22af18ffb47eb94d48e0b6841be3dc2eec85f2d
G1-100 <gavinbh09@gmail.com>
2025-02-05 15:52:29 -0500

    safe four ring bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ void initialize() {
 	pros::delay(1000);
 	optical.set_led_pwm(100);
 	// initializeSelector();  // Commented out selector initialization
-	allianceColorBlue = false; // VERY IMPORTANT
+	allianceColorBlue = true; // VERY IMPORTANT
 	initColorSort();
 	std::cout << "initialize done" << "\n";
 	pros::lcd::initialize();
